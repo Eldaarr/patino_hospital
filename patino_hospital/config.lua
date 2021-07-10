5c29578d916ad0a59ac00d4234ae7b5800e9d5fe
mauriziopatino <46483376+mauriziopatino@users.noreply.github.com>
2021-07-09 22:14:45 -0500

    ESX Legacy = false (default)

--- patino_hospital/config.lua
+++ patino_hospital/config.lua
@@ -4,7 +4,7 @@ Config = {}
 ------------------------------------------ GENERAL STUFF ---------------------------------------------
 ------------------------------------------------------------------------------------------------------ 
 Config.Language = 'en'
-Config.UsingESXLegacy = true -- If you are using ESX Legacy, set this to true and uncomment: '@es_extended/imports.lua' in fxmanifest.lua
+Config.UsingESXLegacy = false -- If you are using ESX Legacy, set this to true and uncomment: '@es_extended/imports.lua' in fxmanifest.lua
 Config.ReviveInvoice = 1500
 Config.UseRprogress = true -- Disable if you want to revive instantly https://forum.cfx.re/t/release-standalone-rprogress-customisable-radial-progress-bars/1630655
 Config.EMSJobName = 'ambulance'
@@ -16,9 +16,13 @@ Config.EMSRequired = 3
 
 Config.PedLocations = {
     {x = 319.20547485352, y = -588.57763671875, z = 43.284042358398, h = 149.54759216309} -- Needs heading
+ -- {x = 111.13213213122, y = -333.32332455522, z = 43.414144222222, h = 322.32424455551} *Make sure to add a comma ^^^^
 }
--- Model Hash
-Config.PedModel = 0xD47303AC
+
+-- You can find PED MODEL and hash here: https://wiki.rage.mp/index.php?title=Peds
+Config.RequestModel = "s_m_m_doctor_01"
+-- ^^ (Hash)
+Config.PedModel = 0xD47303AC 
 
 ------------------------------------------------------------------------------------------------------
 ------------------------------------------ 3D TEXT STUFF ---------------------------------------------
