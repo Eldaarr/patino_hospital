f05a85ae165685a6a2cc2a561b3b1577a1838403
mauriziopatino <46483376+mauriziopatino@users.noreply.github.com>
2021-07-27 15:26:47 -0500

    New locales

--- patino_hospital/locales/locales.lua
+++ patino_hospital/locales/locales.lua
@@ -1,20 +1,22 @@
 Locales = {
 
     ['en'] = {
-        ['press_to_heal'] = "~r~[E]~s~ Visit EMS",
-        ['not_enough_money'] = 'You don\'t have enough money',
-        ['successfully_paid'] = 'The doctor checked you!',
-        ['player_is_not_dead'] = '~r~Player is not dead!',
-        ['enough_ems'] = 'There are a lot of EMS, please search one!'
+        ['press_to_heal']               = "~r~[E]~s~ Visit EMS",
+        ['not_enough_money']            = 'You don\'t have enough money',
+        ['successfully_paid']           = 'The doctor checked you!',
+        ['player_is_not_dead']          = '~r~Player is not dead!',
+        ['enough_ems']                  = 'There are a lot of EMS, please search one!',
+        ['doctor_checking_you']         = 'Doctor is checking you...'
     
     },
 
     ['es'] = {
-        ['press_to_heal'] = "~r~[E]~s~ Visitar EMS",
-        ['not_enough_money'] = 'No tienes suficiente dinero',
-        ['successfully_paid'] = 'El doctor te ha revisado!',
-        ['player_is_not_dead'] = '~r~El jugador no está muerto!',
-        ['enough_ems'] = 'Hay muchos EMS, por favor busca uno!'
+        ['press_to_heal']               = "~r~[E]~s~ Visitar EMS",
+        ['not_enough_money']            = 'No tienes suficiente dinero',
+        ['successfully_paid']           = 'El doctor te ha revisado!',
+        ['player_is_not_dead']          = '~r~El jugador no está muerto!',
+        ['enough_ems']                  = 'Hay muchos EMS, por favor busca uno!',
+        ['doctor_checking_you']         = 'El doctor te está revisando...'
     }
 
 }
